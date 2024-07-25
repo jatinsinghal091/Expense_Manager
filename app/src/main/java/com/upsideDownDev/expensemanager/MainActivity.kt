c782d11df791413ad42b1c43cf8beb56280f88a5
jatin <jatinsinghal404@gmail.com>
2024-07-25 21:56:58 +0530

    initial commit

--- app/src/main/java/com/upsideDownDev/expensemanager/MainActivity.kt
+++ app/src/main/java/com/upsideDownDev/expensemanager/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.upsideDownDev.expensemanager
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,6 +18,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
+
         setContent {
             ExpenseManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
