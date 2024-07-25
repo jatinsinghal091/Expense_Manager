5e31dee8e097d057d804f972424ca4621e813b7f
jatin <jatinsinghal404@gmail.com>
2024-07-25 21:52:04 +0530

    Revert "initial commit"
    This reverts commit ade972e4d18bc6f6785ede3f70171f2c588c4437.

--- app/src/main/java/com/upsideDownDev/expensemanager/MainActivity.kt
+++ app/src/main/java/com/upsideDownDev/expensemanager/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.upsideDownDev.expensemanager
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,11 +17,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            window.isNavigationBarContrastEnforced = false
-        }
-
         setContent {
             ExpenseManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
